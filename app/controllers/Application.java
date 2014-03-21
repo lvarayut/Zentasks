efa30ddee8e0471cc8e3b5510238af64ea68b9ce
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2014-03-21 17:20:30 +0100

    [Add] login page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,9 @@ package controllers;
 
 import models.Project;
 import models.Task;
+import models.User;
 import play.*;
+import play.data.*;
 import play.mvc.*;
 
 import views.html.*;
@@ -14,6 +16,37 @@ public class Application extends Controller {
                 Project.find.all(),
                 Task.find.all()
         ));
+    }
+
+    public static Result login(){
+        return ok(
+                login.render(Form.form(Login.class))
+        );
+    }
+
+    public static Result authenticate(){
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        if(loginForm.hasErrors()){
+            return badRequest(login.render(loginForm));
+        }
+        else{
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(controllers.routes.Application.index());
+        }
+    }
+
+
+    public static class Login{
+        public String email;
+        public String password;
+
+        public String validate(){
+            if (User.authenticate(email, password) == null){
+                return "Invalid user or password";
+            }
+            return null;
+        }
     }
 
 }
